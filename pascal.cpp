ed5c327940704460aee380fc06ac431979845339
1998vk <vk1998@mail.ru>
2017-09-22 14:23:13 +0300

    TODO was done

--- pascal.cpp
+++ pascal.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -13,6 +14,22 @@ int main()
     // 1 3 3 1
     // 1 4 6 4 1
     // ...
+    int n;
+    cin >> n;
+    vector <vector <int> > a(n, vector <int>());
+    for (int i = 0; i < n; i++)
+    {
+	a[i].push_back(1);
+	for (int j = 1; j < i; j++)
+	    a[i].push_back(a[i - 1][j] + a[i - 1][j - 1]);
+	a[i].push_back(1);
+    }
+    for (int i = 0; i < n; i++)
+    {
+	for (int j = 0; j <= i; j++)
+	    cout << a[i][j] << " ";
+	cout << endl;
+    }
     return 0;
 }
 
